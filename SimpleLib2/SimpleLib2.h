511497093bf7eaa23830d417abbb049479a95152
Eric Chaves <eric@craftti.com.br>
2013-12-11 17:09:38 -0200

    sample lib project created

---
+++ SimpleLib2/SimpleLib2.h
@@ -0,0 +1,13 @@
+//
+//  SimpleLib2.h
+//  SimpleLib2
+//
+//  Created by Eric Chaves on 11/12/13.
+//  Copyright (c) 2013 Craftti Solucoes e Projetos Ltda. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface SimpleLib2 : NSObject
+
+@end
---
+++ SimpleLib2/SimpleLib2.m
@@ -0,0 +1,13 @@
+//
+//  SimpleLib2.m
+//  SimpleLib2
+//
+//  Created by Eric Chaves on 11/12/13.
+//  Copyright (c) 2013 Craftti Solucoes e Projetos Ltda. All rights reserved.
+//
+
+#import "SimpleLib2.h"
+
+@implementation SimpleLib2
+
+@end
